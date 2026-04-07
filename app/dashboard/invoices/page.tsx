cda5e0f35b71795dfa4bcab5e96d3d406eddbc03
m0nch1 <m0nch1.develop@gmail.com>
2026-04-07 11:14:35 +0900

    save data exmaple

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,10 +1,21 @@
-import NextLink from 'next/link';
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import { lusitana } from "@/app/ui/fonts";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { fetchLatestInvoices } from "@/app/lib/data";
 
-export default function Page() {
+export default async function Page() {
+  const latestInvoices = await fetchLatestInvoices();
   return (
-    <>
-      <h1>Invoices</h1>
-      <NextLink href="/dashboard">dashboard</NextLink>
-    </>
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <CreateInvoice />
+      </div>
+    </div>
   );
 }
